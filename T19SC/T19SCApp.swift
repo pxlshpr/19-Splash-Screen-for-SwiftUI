04c7d5773628d9b5b9d3039e02b69e9e9f1f6051
MING <54872601+1998code@users.noreply.github.com>
2025-02-09 23:03:53 +0800

    Initial Commit

---
+++ T19SC/T19SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  T19SCApp.swift
+//  T19SC
+//
+//  Created by Ming on 9/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct T19SCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
